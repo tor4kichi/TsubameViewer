54bcc77c2ea66bce4b7e5cc8169f4f71821e41cc
tor4kichi <tor4kichi@gmail.com>
2022-02-07 08:59:08 +0900

    MostRecentlyUsedList等はUIスレッド上でアクセスする必要がある。

--- TsubameViewer/TsubameViewer/Models.UseCase/Maintenance/RemoveSourceStorageItemWhenPathIsEmpty.cs
+++ TsubameViewer/TsubameViewer/Models.UseCase/Maintenance/RemoveSourceStorageItemWhenPathIsEmpty.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Concurrency;
 using System.Text;
 using System.Threading.Tasks;
 using TsubameViewer.Models.Domain.SourceFolders;
@@ -12,41 +13,53 @@ namespace TsubameViewer.Models.UseCase.Maintenance
     public sealed class RemoveSourceStorageItemWhenPathIsEmpty : ILaunchTimeMaintenanceAsync
     {
         private readonly ILiteDatabase _liteDatabase;
+        private readonly IScheduler _scheduler;
 
-        public RemoveSourceStorageItemWhenPathIsEmpty(ILiteDatabase liteDatabase)
+        public RemoveSourceStorageItemWhenPathIsEmpty(
+            ILiteDatabase liteDatabase,
+            IScheduler scheduler
+            )
         {
             _liteDatabase = liteDatabase;
+            _scheduler = scheduler;
         }
 
-        async Task ILaunchTimeMaintenanceAsync.MaintenanceAsync()
+        Task ILaunchTimeMaintenanceAsync.MaintenanceAsync()
         {
-            try
-            {
-                if (_liteDatabase.CollectionExists(nameof(IgnoreStorageItemEntry)))
-                {
-                    var removeCount = _liteDatabase.GetCollection<IgnoreStorageItemEntry>().DeleteMany(x => x.Path == String.Empty);
-                }
-            }
-            catch
-            {
-                _liteDatabase.DropCollection(nameof(IgnoreStorageItemEntry));
-            }
-
-            foreach (var entry in StorageApplicationPermissions.MostRecentlyUsedList.Entries)
+            TaskCompletionSource tcs = new TaskCompletionSource();
+            _scheduler.Schedule(async () => 
             {
                 try
                 {
-                    var item = await StorageApplicationPermissions.MostRecentlyUsedList.GetItemAsync(entry.Token, AccessCacheOptions.FastLocationsOnly);
-                    if (string.IsNullOrEmpty(item.Path))
+                    if (_liteDatabase.CollectionExists(nameof(IgnoreStorageItemEntry)))
                     {
-                        StorageApplicationPermissions.MostRecentlyUsedList.Remove(entry.Token);
+                        var removeCount = _liteDatabase.GetCollection<IgnoreStorageItemEntry>().DeleteMany(x => x.Path == String.Empty);
                     }
                 }
                 catch
                 {
-
+                    _liteDatabase.DropCollection(nameof(IgnoreStorageItemEntry));
                 }
-            }
+
+                foreach (var entry in StorageApplicationPermissions.MostRecentlyUsedList.Entries)
+                {
+                    try
+                    {
+                        var item = await StorageApplicationPermissions.MostRecentlyUsedList.GetItemAsync(entry.Token, AccessCacheOptions.FastLocationsOnly);
+                        if (string.IsNullOrEmpty(item.Path))
+                        {
+                            StorageApplicationPermissions.MostRecentlyUsedList.Remove(entry.Token);
+                        }
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                tcs.SetResult();
+            });
+
+            return tcs.Task;
         }
     }
 }
