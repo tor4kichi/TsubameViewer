47d287318ab203ed8e585c854a139659229f6477
tor4kichi <tor4kichi@gmail.com>
2026-06-10 12:01:38 +0900

    フォルダアイテムのアイテムメニューから読了状態を手動で変更できるように。

--- TsubameViewer/Views/Flyouts/StorageItemMenuFlyout.xaml.cs
+++ TsubameViewer/Views/Flyouts/StorageItemMenuFlyout.xaml.cs
@@ -1,10 +1,20 @@
 ﻿using CommunityToolkit.Mvvm.DependencyInjection;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using I18NPortable;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using TsubameViewer.Contracts.Notification;
 using TsubameViewer.Core.Contracts.Services;
 using TsubameViewer.Core.Models;
 using TsubameViewer.Core.Models.Albam;
+using TsubameViewer.Core.Models.FolderItemListing;
 using TsubameViewer.Core.Models.ImageViewer.ImageSource;
-using TsubameViewer.ViewModels.Albam.Commands;
+using TsubameViewer.Core.Models.SourceFolders;
 using TsubameViewer.ViewModels;
+using TsubameViewer.ViewModels.Albam.Commands;
 using TsubameViewer.ViewModels.PageNavigation.Commands;
 using TsubameViewer.ViewModels.SourceFolders.Commands;
 using TsubameViewer.Views.Helpers;
@@ -12,12 +22,8 @@ using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
-using TsubameViewer.Core.Models.SourceFolders;
-using System.IO;
-using CommunityToolkit.Mvvm.Messaging;
 
-// ユーザー コントロールの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234236 を参照してください
-
+#nullable enable
 namespace TsubameViewer.Views.Flyouts;
 
 public sealed partial class StorageItemMenuFlyout : MenuFlyout
@@ -43,20 +49,24 @@ public sealed partial class StorageItemMenuFlyout : MenuFlyout
         OpenWithExplorerItem.Command = Ioc.Default.GetService<OpenWithExplorerCommand>();
         SelectedItems_OpenWithExplorerItem.Command = OpenWithExplorerItem.Command;
         OpenWithExternalAppMenuItem.Command = Ioc.Default.GetService<OpenWithExternalApplicationCommand>();
-        _secondaryTileManager = Ioc.Default.GetService<ISecondaryTileManager>();
-        _favoriteAlbam = Ioc.Default.GetService<FavoriteAlbam>();
-        _albamRepository = Ioc.Default.GetService<AlbamRepository>();
+        _secondaryTileManager = Ioc.Default.GetRequiredService<ISecondaryTileManager>();
+        _favoriteAlbam = Ioc.Default.GetRequiredService<FavoriteAlbam>();
+        _albamRepository = Ioc.Default.GetRequiredService<AlbamRepository>();
+        _bookmarkRepository = Ioc.Default.GetRequiredService<LocalBookmarkRepository>();
+        ToggleReadingFinishedStateItem.Command = ToggleReadingFinishedStateCommand;
     }
 
     readonly ISecondaryTileManager _secondaryTileManager;
     readonly FavoriteAlbam _favoriteAlbam;
     readonly AlbamRepository _albamRepository;
-    
+    readonly LocalBookmarkRepository _bookmarkRepository;
+
     void FolderAndArchiveMenuFlyout_Opened(object sender, object e)
     {
-        var flyout = sender as FlyoutBase;
-        
-        IStorageItemViewModel itemVM = flyout.Target.DataContext as IStorageItemViewModel;
+        long time = TimeProvider.System.GetTimestamp();
+
+        var flyout = (FlyoutBase)sender;
+        IStorageItemViewModel? itemVM = flyout.Target.DataContext as IStorageItemViewModel;
         if (itemVM == null && flyout.Target is Control content)
         {
             itemVM = (content as ContentControl)?.Content as IStorageItemViewModel;
@@ -307,5 +317,36 @@ public sealed partial class StorageItemMenuFlyout : MenuFlyout
                 }
             }
         }
+
+        if (itemVM.Type is Core.Models.StorageItemTypes.Archive or Core.Models.StorageItemTypes.EBook or Core.Models.StorageItemTypes.Movie)
+        {
+            var facade = _bookmarkRepository.GetBookmarkFacade(itemVM.Path);
+            ToggleReadingFinishedStateItem.CommandParameter = itemVM;
+            ToggleReadingFinishedStateItem.Text = facade.IsFinishedReading
+                ? "ReadingState_ToggleToUnfinished".Translate()
+                : "ReadingState_ToggleToFinished".Translate();
+            ToggleReadingFinishedStateItem.Visibility = Visibility.Visible;
+        }
+        else
+        {
+            ToggleReadingFinishedStateItem.Visibility = Visibility.Collapsed;
+        }
+        
+
+        Debug.WriteLine($"StorateItemMenuFlyout.Opened: {TimeProvider.System.GetElapsedTime(time)}");
+    }
+
+
+    [RelayCommand]
+    void ToggleReadingFinishedState(IStorageItemViewModel? itemVM)
+    {
+        if (itemVM == null) { return; }
+
+        var facade = _bookmarkRepository.GetBookmarkFacade(itemVM.Path);
+        facade.IsFinishedReading = !facade.IsFinishedReading;
+        itemVM.UpdateLastReadPosition();
+        Ioc.Default.GetRequiredService<IMessenger>().SendShowTextNotificationMessage(facade.IsFinishedReading
+            ? "ReadingState_SetToFinished".Translate()
+            : "ReadingState_SetToUnfinished".Translate());
     }
 }
